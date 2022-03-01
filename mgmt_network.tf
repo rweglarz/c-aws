d0900e59cf02551de1b8d0ec345f8971b42a3d23
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2022-03-01 20:38:27 +0100

    Add tgw and connect mgmt to it

--- mgmt_network.tf
+++ mgmt_network.tf
@@ -38,10 +38,11 @@ resource "aws_route" "mgmt-dg" {
   route_table_id         = aws_route_table.mgmt.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.mgmt.id
-  }
-  tags = {
-    Name = "${var.name}-mgmt-rt"
-  }
+}
+resource "aws_route" "mgmt-private" {
+  route_table_id         = aws_route_table.mgmt.id
+  destination_cidr_block = "172.16.0.0/12"
+  transit_gateway_id     = aws_ec2_transit_gateway.tgw.id
 }
 
 
@@ -122,4 +123,34 @@ resource "aws_eip" "jumphost" {
   tags = {
     Name = "${var.name}-jumphost"
   }
+}
+
+
+resource "aws_ec2_transit_gateway_vpc_attachment" "mgmt" {
+  vpc_id                                          = aws_vpc.mgmt.id
+  subnet_ids                                      = aws_subnet.mgmt[*].id
+  transit_gateway_id                              = aws_ec2_transit_gateway.tgw.id
+  transit_gateway_default_route_table_association = false
+  transit_gateway_default_route_table_propagation = false
+  tags = {
+    Name = "${var.name}-mgmt"
+  }
+}
+
+resource "aws_ec2_transit_gateway_route_table" "mgmt" {
+  transit_gateway_id = aws_ec2_transit_gateway.tgw.id
+  tags = {
+    Name = "${var.name}-mgmt"
+  }
+}
+
+resource "aws_ec2_transit_gateway_route_table_association" "mgmt" {
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.mgmt.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.mgmt.id
+}
+
+resource "aws_ec2_transit_gateway_route" "mgmt-dg" {
+  destination_cidr_block         = "0.0.0.0/0"
+  transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.mgmt.id
+  transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.mgmt.id
 }
