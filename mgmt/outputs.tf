5b9405a959121540552781528f6e820005e7de0b
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2022-06-26 19:05:05 +0200

    style: fix output

--- mgmt/outputs.tf
+++ mgmt/outputs.tf
@@ -1,12 +1,12 @@
-output aws_vpc_mgmt_id {
+output "aws_vpc_mgmt_id" {
   value = aws_vpc.mgmt.id
 }
 
-output aws_subnet_mgmt_id {
+output "aws_subnet_mgmt_id" {
   value = aws_subnet.mgmt[*].id
 }
 
-output aws_route_table_mgmt_id {
+output "aws_route_table_mgmt_id" {
   value = aws_route_table.mgmt.id
 }
 
