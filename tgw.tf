9624e805b56cf2ea31c23bf28d5e595348762eb4
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2022-06-26 16:31:29 +0200

    refactor: remove old tgw file

--- tgw.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_ec2_transit_gateway" "tgw" {
-  default_route_table_association = "disable"
-  default_route_table_propagation = "disable"
-  tags = {
-    Name = "${var.name}-tgw"
-  }
-}
