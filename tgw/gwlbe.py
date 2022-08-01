2736954a49c6aaad9b840ee5c0a6e611356aafd0
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2022-08-01 21:26:59 +0200

    chore: formatting

--- tgw/gwlbe.py
+++ tgw/gwlbe.py
@@ -5,20 +5,14 @@ import boto3
 import copy
 import json
 from lxml import etree
-from lxml.builder import E 
+from lxml.builder import E
 import requests
 import re
 import sys
 
 import urllib3
-urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-# 1. query vpce on aws, get zone from tag
-# 2. query panorama and gets the zone to interface mapping
-# 3. map the vpce via zone to interface
-# 4. update the launch template with vpce mappings for the new firewalls
-# 5. get the existing / connected fws from panorama
-# 6. update the existing firewalls with the vpce mappings
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
 region = 'eu-central-1'
 
@@ -37,12 +31,13 @@ def getDGMembers(dg):
     s.text = dg
     params['cmd'] = etree.tostring(r)
     serials = []
-    devices = etree.fromstring(requests.get(pano_base_url, params=params, verify=False).content)
+    devices = etree.fromstring(
+        requests.get(pano_base_url, params=params, verify=False).content)
     for device in devices.findall('.//devices/'):
         serial = device.find('serial').text
         connected = device.find('connected').text
-        if connected=="no":
-          continue
+        if connected == "no":
+            continue
         serials.append(serial)
     return serials
 
@@ -57,23 +52,25 @@ def getSysInfo(serials):
         params['target'] = s
         print(requests.get(pano_base_url, params=params, verify=False).content)
 
+
 def parseShowPluginsVmseriesAwsGwlb(txt):
-  sm = 0
-  mappings = {}
-  for l in txt.splitlines():
-      if "GWLB enabled" in l:
-          if not "True" in l:
-            print("WARNING: GWLB not enabled")
-          continue
-      if "VPC endpoint" in l:
-          sm = 1
-          continue
-      if sm !=1:
-        continue
-      rm = re.match(r'\s+(vpce-[a-f0-9]+)\s+(ethernet.*[0-9])\s*$', l)
-      if rm:
-        mappings[rm.group(1)] = rm.group(2)
-  return mappings
+    sm = 0
+    mappings = {}
+    for l in txt.splitlines():
+        if "GWLB enabled" in l:
+            if not "True" in l:
+                print("WARNING: GWLB not enabled")
+            continue
+        if "VPC endpoint" in l:
+            sm = 1
+            continue
+        if sm != 1:
+            continue
+        rm = re.match(r'\s+(vpce-[a-f0-9]+)\s+(ethernet.*[0-9])\s*$', l)
+        if rm:
+            mappings[rm.group(1)] = rm.group(2)
+    return mappings
+
 
 def getFirewallsExistingVpceMappings(serial):
     params = copy.copy(base_params)
@@ -86,12 +83,13 @@ def getFirewallsExistingVpceMappings(serial):
     params['target'] = serial
     resp = requests.get(pano_base_url, params=params, verify=False).content
     xml_resp = etree.fromstring(resp)
-    if not xml_resp.attrib.get('status')=='success':
+    if not xml_resp.attrib.get('status') == 'success':
         print("Failed to get mappings from {}".format(serial))
         print(resp)
         return {}
     m = parseShowPluginsVmseriesAwsGwlb(xml_resp.find('./result').text)
     return m
+
 
 def endpointMappingXML(pvpce, pinterface, associate):
     r = etree.Element('request')
@@ -100,9 +98,9 @@ def endpointMappingXML(pvpce, pinterface, associate):
     s = etree.SubElement(s, 'aws')
     s = etree.SubElement(s, 'gwlb')
     if associate:
-      s = etree.SubElement(s, 'associate')
+        s = etree.SubElement(s, 'associate')
     else:
-      s = etree.SubElement(s, 'disassociate')
+        s = etree.SubElement(s, 'disassociate')
     vpce = etree.SubElement(s, 'vpc-endpoint')
     vpce.text = pvpce
     interface = etree.SubElement(s, 'interface')
@@ -110,17 +108,20 @@ def endpointMappingXML(pvpce, pinterface, associate):
     #print(etree.tostring(r, pretty_print=True).decode())
     return etree.tostring(r)
 
+
 def addVpceMappingToFirewall(serial, vpce, interface):
     params = copy.copy(base_params)
     params['target'] = serial
     params['cmd'] = endpointMappingXML(vpce, interface, True)
     resp = requests.get(pano_base_url, params=params, verify=False).content
     xml_resp = etree.fromstring(resp)
-    if not xml_resp.attrib.get('status')=='success':
-        print("Failed to create mapping {} {} on {}".format(vpce, interface, serial))
+    if not xml_resp.attrib.get('status') == 'success':
+        print("Failed to create mapping {} {} on {}".format(
+            vpce, interface, serial))
         print(resp)
         return False
     return True
+
 
 def deleteVpceMappingFromFirewall(serial, vpce, interface):
     params = copy.copy(base_params)
@@ -128,135 +129,157 @@ def deleteVpceMappingFromFirewall(serial, vpce, interface):
     params['cmd'] = endpointMappingXML(vpce, interface, False)
     resp = requests.get(pano_base_url, params=params, verify=False).content
     xml_resp = etree.fromstring(resp)
-    if not xml_resp.attrib.get('status')=='success':
-        print("Failed to create mapping {} {} on {}".format(vpce, interface, serial))
+    if not xml_resp.attrib.get('status') == 'success':
+        print("Failed to create mapping {} {} on {}".format(
+            vpce, interface, serial))
         print(resp)
         return False
     return True
 
+
 def manageVpceMappingsOnFirewall(serial, mappings):
     existing_mappings = getFirewallsExistingVpceMappings(serial)
     for v in mappings:
-      if v in existing_mappings:
-        if mappings[v]==existing_mappings[v]:
-          continue
-      if addVpceMappingToFirewall(serial, v, mappings[v]):
-        print("Created mapping {} {}".format(v, mappings[v]))
+        if v in existing_mappings:
+            if mappings[v] == existing_mappings[v]:
+                continue
+        if addVpceMappingToFirewall(serial, v, mappings[v]):
+            print("Created mapping {} {}".format(v, mappings[v]))
     for v in existing_mappings:
-      if v in mappings:
-        continue
-      print("Firewall has extra mapping {}".format(v))
-      if deleteVpceMappingFromFirewall(serial, v, existing_mappings[v]):
-        print("Removed mapping {} {}".format(v, existing_mappings[v]))
+        if v in mappings:
+            continue
+        print("Firewall has extra mapping {}".format(v))
+        if deleteVpceMappingFromFirewall(serial, v, existing_mappings[v]):
+            print("Removed mapping {} {}".format(v, existing_mappings[v]))
+
 
 def manageVpceMappingsOnActiveFirewalls(serials, mappings):
-    if len(serials)==0:
-      print("No connected serials found")
-      return
+    if len(serials) == 0:
+        print("No connected serials found")
+        return
     for s in serials:
         print("Populating vpce mappings on {}".format(s))
         manageVpceMappingsOnFirewall(s, mappings)
+
 
 def addVpceMappingsToLaunchTemplate(ltn, mappings):
     lt_po = 'plugin-op-commands=panorama-licensing-mode-on,aws-gwlb-inspect:enable'
     client = boto3.client('ec2', region_name=region)
     di = client.describe_launch_template_versions(LaunchTemplateName=ltn,
-      Versions=['$Latest'])
+                                                  Versions=['$Latest'])
     for v in di.get('LaunchTemplateVersions'):
-      latest_ver = v.get('VersionNumber')
-      ud = v.get('LaunchTemplateData').get('UserData')
-      lt = base64.b64decode(ud).decode()
+        latest_ver = v.get('VersionNumber')
+        ud = v.get('LaunchTemplateData').get('UserData')
+        lt = base64.b64decode(ud).decode()
     nlt = ''
     for l in lt.splitlines():
-      m = re.match(r'plugin-op-commands.*', l)
-      if m:
-        nlt+= lt_po
-        for v in mappings:
-          nlt+= ',aws-gwlb-associate-vpce:{}@{}'.format(v, mappings[v])
-        nlt+= '\n'
-      else:
-        nlt+= l + '\n'
+        m = re.match(r'plugin-op-commands.*', l)
+        if m:
+            nlt += lt_po
+            for v in mappings:
+                nlt += ',aws-gwlb-associate-vpce:{}@{}'.format(v, mappings[v])
+            nlt += '\n'
+        else:
+            nlt += l + '\n'
     di = client.create_launch_template_version(LaunchTemplateName=ltn,
-      LaunchTemplateData={'UserData':base64.b64encode(nlt.encode()).decode()},
-      SourceVersion=str(latest_ver))
+                                               LaunchTemplateData={
+                                                   'UserData':
+                                                   base64.b64encode(
+                                                       nlt.encode()).decode()
+                                               },
+                                               SourceVersion=str(latest_ver))
     v = di.get('LaunchTemplateVersion').get('VersionNumber')
     print("set new version to: {}".format(v))
-    client.modify_launch_template(LaunchTemplateName=ltn, DefaultVersion=str(v))
+    client.modify_launch_template(LaunchTemplateName=ltn,
+                                  DefaultVersion=str(v))
+
 
 def getAwsVpce():
-  client = boto3.client('ec2', region_name=region) 
-  dv = client.describe_vpc_endpoints()
-  vpce_zone = {}
-  for e in dv.get('VpcEndpoints'):
-    if e.get('VpcEndpointType')!='GatewayLoadBalancer':
-      continue
-    vpce = e.get('VpcEndpointId')
-    for t in e.get('Tags'):
-      if t['Key']=='pan_zone':
-        zone = t['Value']
-        vpce_zone[vpce] = zone
-        break
-    else:
-      print("Did not find zone tag for {}".format(vpce))
-  return vpce_zone
+    client = boto3.client('ec2', region_name=region)
+    dv = client.describe_vpc_endpoints()
+    vpce_zone = {}
+    for e in dv.get('VpcEndpoints'):
+        if e.get('VpcEndpointType') != 'GatewayLoadBalancer':
+            continue
+        vpce = e.get('VpcEndpointId')
+        for t in e.get('Tags'):
+            if t['Key'] == 'pan_zone':
+                zone = t['Value']
+                vpce_zone[vpce] = zone
+                break
+        else:
+            print("Did not find zone tag for {}".format(vpce))
+    return vpce_zone
+
 
 def getPanoramaZoneInterfaceMapping(template):
-  p = "/config/devices/entry[@name='localhost.localdomain']/"
-  p+= "template/entry[@name='{}']/".format(template)
-  p+= "config/devices/entry[@name='localhost.localdomain']/"
-  p+= "vsys/entry[@name='vsys1']"
-  params = copy.copy(base_params)
-  params['type'] = 'config'
-  params['action'] = 'get'
-  params['xpath'] = p
-  r = etree.fromstring(requests.get(pano_base_url, params=params, verify=False).content)
-  m = {}
-  for z in r.findall('.//entry/zone/entry'):
-    zone = z.attrib.get('name')
-    for i in z.findall('./network/layer3/member'):
-      interface = i.text
-      m[zone] = interface
-  return m
+    p = "/config/devices/entry[@name='localhost.localdomain']/"
+    p += "template/entry[@name='{}']/".format(template)
+    p += "config/devices/entry[@name='localhost.localdomain']/"
+    p += "vsys/entry[@name='vsys1']"
+    params = copy.copy(base_params)
+    params['type'] = 'config'
+    params['action'] = 'get'
+    params['xpath'] = p
+    r = etree.fromstring(
+        requests.get(pano_base_url, params=params, verify=False).content)
+    m = {}
+    for z in r.findall('.//entry/zone/entry'):
+        zone = z.attrib.get('name')
+        for i in z.findall('./network/layer3/member'):
+            interface = i.text
+            m[zone] = interface
+    return m
+
 
 def createDummyEndpointMappings():
-  mappings = {}
-  for i in range(1,251):
-    a = "vpce-{:>010}".format(i)
-    if i%2==0:
-      mappings[a] = 'ethernet1/1.1'
-    else:
-      mappings[a] = 'ethernet1/1.2'
-  return mappings
+    mappings = {}
+    for i in range(1, 251):
+        a = "vpce-{:>010}".format(i)
+        if i % 2 == 0:
+            mappings[a] = 'ethernet1/1.1'
+        else:
+            mappings[a] = 'ethernet1/1.2'
+    return mappings
+
 
 def readConfiguration():
-  global pano_base_url
-  with open("panorama_creds.json") as f:
-    data = json.load(f)
-    base_params["key"] = data["api_key"]
-    pano_base_url = 'https://{}/api/'.format(data['hostname'])
+    global pano_base_url
+    with open("panorama_creds.json") as f:
+        data = json.load(f)
+        base_params["key"] = data["api_key"]
+        pano_base_url = 'https://{}/api/'.format(data['hostname'])
+
 
 def mapVpceToInterface(endpoint_zone_mapping, interface_zone_mapping):
-  endpoint_interface_mapping = {}
-  for vpce in endpoint_zone_mapping:
-    z = endpoint_zone_mapping[vpce]
-    if not z in interface_zone_mapping:
-      print("No interface with zone {} exist in panorama template for {}".format(z, vpce))
-      continue
-    endpoint_interface_mapping[vpce] = interface_zone_mapping[z]
-  return endpoint_interface_mapping
+    endpoint_interface_mapping = {}
+    for vpce in endpoint_zone_mapping:
+        z = endpoint_zone_mapping[vpce]
+        if not z in interface_zone_mapping:
+            print(
+                "No interface with zone {} exist in panorama template for {}".
+                format(z, vpce))
+            continue
+        endpoint_interface_mapping[vpce] = interface_zone_mapping[z]
+    return endpoint_interface_mapping
+
 
 def main():
-  readConfiguration()
-  #mappings = createDummyEndpointMappings()
-  #getSysInfo(serials)
-  endpoint_zone_mapping = getAwsVpce()
-  interface_zone_mapping = getPanoramaZoneInterfaceMapping('aws-gwlb')
-  ei = mapVpceToInterface(endpoint_zone_mapping, interface_zone_mapping)
-  print(ei)
-  addVpceMappingsToLaunchTemplate('m-mfw', ei)
-  serials = getDGMembers("awsgwlbvmseries")
-  manageVpceMappingsOnActiveFirewalls(serials, ei)
-
+    readConfiguration()
+    #getSysInfo(serials)
+    # 1. query vpce on aws, get zone from tag
+    endpoint_zone_mapping = getAwsVpce()
+    # 2. query panorama and gets the zone to interface mapping
+    interface_zone_mapping = getPanoramaZoneInterfaceMapping('aws-gwlb')
+    # 3. map the vpce via zone to interface
+    ei = mapVpceToInterface(endpoint_zone_mapping, interface_zone_mapping)
+    print(ei)
+    # 4. update the launch template with vpce mappings for the new firewalls
+    addVpceMappingsToLaunchTemplate('m-mfw', ei)
+    # 5. get the existing / connected fws from panorama
+    serials = getDGMembers("awsgwlbvmseries")
+    # 6. update the existing firewalls with the vpce mappings
+    manageVpceMappingsOnActiveFirewalls(serials, ei)
 
 
 if __name__ == '__main__':
