6d76531c34b599809f92f88e8ac8e68d6f26fb0b
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2022-10-28 16:43:30 +0200

    fix: accept tgw-peer attachment, add explicit dependencies

--- tgw-peer/tgw-p.tf
+++ tgw-peer/tgw-p.tf
@@ -25,38 +25,52 @@ resource "aws_ec2_transit_gateway_peering_attachment" "main-tgw" {
   }
 }
 
+resource "aws_ec2_transit_gateway_peering_attachment_accepter" "main-tgw" {
+  provider                      = aws.peer
+  transit_gateway_attachment_id = aws_ec2_transit_gateway_peering_attachment.main-tgw.id
+}
+
 resource "aws_ec2_transit_gateway_route" "local-to-peer-app0" {
   destination_cidr_block         = "172.31.200.0/24"
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.main-tgw.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.spoke.id
+  depends_on = [
+    aws_ec2_transit_gateway_peering_attachment_accepter.main-tgw
+  ]
 }
 resource "aws_ec2_transit_gateway_route" "local-to-peer-app1" {
   destination_cidr_block         = "172.31.201.0/24"
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.main-tgw.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.spoke.id
+  depends_on = [
+    aws_ec2_transit_gateway_peering_attachment_accepter.main-tgw
+  ]
 }
 
-/*
-resource "aws_ec2_transit_gateway_peering_attachment_accepter" "main-tgw" {
-  provider                      = aws.peer
-  transit_gateway_attachment_id = aws_ec2_transit_gateway_peering_attachment.main-tgw.id
-}
-*/
 resource "aws_ec2_transit_gateway_route" "peer-to-local-app5" {
   provider                       = aws.peer
   destination_cidr_block         = "172.31.205.0/24"
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.main-tgw.id
   transit_gateway_route_table_id = data.terraform_remote_state.tgw.outputs.transit_gateway_route_tables["sec"]
+  depends_on = [
+    aws_ec2_transit_gateway_peering_attachment_accepter.main-tgw
+  ]
 }
 resource "aws_ec2_transit_gateway_route_table_association" "peer-sec" {
   provider                       = aws.peer
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.main-tgw.id
   transit_gateway_route_table_id = data.terraform_remote_state.tgw.outputs.transit_gateway_route_tables["sec"]
+  depends_on = [
+    aws_ec2_transit_gateway_peering_attachment_accepter.main-tgw
+  ]
 }
 
 resource "aws_ec2_transit_gateway_route_table_association" "peer" {
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_peering_attachment.main-tgw.id
   transit_gateway_route_table_id = aws_ec2_transit_gateway_route_table.spoke.id
+  depends_on = [
+    aws_ec2_transit_gateway_peering_attachment_accepter.main-tgw
+  ]
 }
 resource "aws_ec2_transit_gateway_route_table_propagation" "app5_to_spoke" {
   transit_gateway_attachment_id  = module.vpc-app5.transit_gateway_attachment_id
