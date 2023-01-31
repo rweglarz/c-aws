db9842ddbe082b291b50e3e1430ec92319f26fd3
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2023-01-31 22:37:41 +0100

    feat: cn: add ebs-csi for pv

--- cn/eks.tf
+++ cn/eks.tf
@@ -10,6 +10,10 @@ module "eks_c1" {
   ]
 
   cluster_addons = {
+    aws-ebs-csi-driver = {
+      most_recent              = true
+      service_account_role_arn = module.ebs_csi_irsa_role.iam_role_arn
+    }
     # coredns = {
     #   most_recent = true
     # }
@@ -78,6 +82,22 @@ module "eks_c1" {
     }
   }
 }
+
+
+module "ebs_csi_irsa_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name             = "ebs-csi"
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks_c1.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+
 
 
 provider "kubernetes" {
