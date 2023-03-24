dc239d0b3d2bb128fb0afb62e4b00eed91701b34
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2023-03-24 18:40:25 +0100

    chore: modules/gwlb: print / formatting

--- modules/gwlb_asg_fw/lambda.py
+++ modules/gwlb_asg_fw/lambda.py
@@ -11,13 +11,12 @@ def lambda_handler(event, context):
     if os.environ['subnet_ids']:
         subnet_ids = os.environ['subnet_ids'].split(",")
     else:
-        log("Empty Environment variable subnet_ids:" +
-            os.environ['subnet_ids'])
+        log("Empty Environment variable subnet_ids:".format(os.environ['subnet_ids']))
         exit(1)
     if os.environ['sg_ids']:
         sg_ids = os.environ['sg_ids'].split(",")
     else:
-        log("Empty Environment variable sg_ids:" + os.environ['sg_ids'])
+        log("Empty Environment variable sg_ids:".format(os.environ['sg_ids']))
         exit(1)
 
     if event["detail-type"] != "EC2 Instance-launch Lifecycle Action":
@@ -38,38 +37,41 @@ def lambda_handler(event, context):
         availability_zone = zones["Subnets"][0]["AvailabilityZone"]
         if availability_zone != instance_zone:
             continue
-        log("Adding interface index:{} in subnet: {}".format(
-            device_index, subnet))
+        log("Adding interface index:{} in subnet: {}".format(device_index, subnet))
 
         network_interface = ec2_client.create_network_interface(
-            SubnetId=subnet, Groups=sg_ids)
-        network_interface_id = network_interface['NetworkInterface'][
-            'NetworkInterfaceId']
+                SubnetId = subnet,
+                Groups = sg_ids,
+                Description = subnet,
+            )
+        network_interface_id = network_interface['NetworkInterface']['NetworkInterfaceId']
         log("Created network interface: {}".format(network_interface_id))
 
         attach_interface = ec2_client.attach_network_interface(
-            NetworkInterfaceId=network_interface_id,
-            InstanceId=instance_id,
-            DeviceIndex=device_index)
+                NetworkInterfaceId=network_interface_id,
+                InstanceId=instance_id,
+                DeviceIndex=device_index,
+            )
         attachment_id = attach_interface['AttachmentId']
         log("Created attachment: {}".format(attachment_id))
 
         mnia = ec2_client.modify_network_interface_attribute(
-            Attachment={
-                'AttachmentId': attachment_id,
-                'DeleteOnTermination': True,
-            },
-            NetworkInterfaceId=network_interface_id,
-        )
+                Attachment={
+                    'AttachmentId': attachment_id,
+                    'DeleteOnTermination': True,
+                },
+                NetworkInterfaceId=network_interface_id,
+            )
         log("debug: delete on termination: {}".format(mnia))
         device_index += 1
 
     try:
         asg_client.complete_lifecycle_action(
-            LifecycleHookName=event['detail']['LifecycleHookName'],
-            AutoScalingGroupName=event['detail']['AutoScalingGroupName'],
-            LifecycleActionToken=event['detail']['LifecycleActionToken'],
-            LifecycleActionResult='CONTINUE')
+                LifecycleHookName=event['detail']['LifecycleHookName'],
+                AutoScalingGroupName=event['detail']['AutoScalingGroupName'],
+                LifecycleActionToken=event['detail']['LifecycleActionToken'],
+                LifecycleActionResult='CONTINUE',
+            )
 
     except botocore.exceptions.ClientError as e:
         log("Error completing life cycle hook for instance {}: {}".format(
