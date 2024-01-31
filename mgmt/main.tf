6554456516c7507d8cdd4631f5fa99b8a91f8cca
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2024-01-31 18:26:27 +0100

    chore: mgmt: tune the pl entry count

--- mgmt/main.tf
+++ mgmt/main.tf
@@ -47,13 +47,13 @@ module "pl-eu_west_3-mgmt_ips" {
 resource "aws_ec2_managed_prefix_list" "csp_nat_ips" {
   name           = "${var.name} csp permitted incoming IPs"
   address_family = "IPv4"
-  max_entries    = 15
+  max_entries    = 25
 }
 
 resource "aws_ec2_managed_prefix_list" "tmp_ips" {
   name           = "${var.name} public permitted tmp IPs"
   address_family = "IPv4"
-  max_entries    = 15
+  max_entries    = 5
 
   dynamic "entry" {
     for_each = var.tmp_ips
