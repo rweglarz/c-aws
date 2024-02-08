bf306e213be2be7962bc7621e6ea3e0c1cf5d056
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2024-02-08 18:02:17 +0100

    feat: modules/vmseries: add output mgmt_public_ip

--- modules/vmseries/outputs.tf
+++ modules/vmseries/outputs.tf
@@ -1,9 +1,15 @@
 output "public_ips" {
   value = { for k, v in aws_eip.this : k => v.public_ip }
 }
+
+output "mgmt_public_ip" {
+  value = one([for k,v in aws_eip.this: v.public_ip if strcontains(k, "mgmt")])
+}
+
 output "private_ip_list" {
   value = { for k, v in aws_network_interface.this : k => v.private_ip_list }
 }
+
 output "eni" {
   value = { for k, v in aws_network_interface.this : k => v.id }
 }
