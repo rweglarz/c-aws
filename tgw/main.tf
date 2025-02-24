fffaef683b38f3d1ec0678f175f49e1eb20ba4db
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2025-02-24 16:33:32 +0100

    chore: tgw: add provider versions

--- tgw/main.tf
+++ tgw/main.tf
@@ -8,3 +8,19 @@ data "terraform_remote_state" "mgmt" {
     path = "../mgmt/terraform.tfstate"
   }
 }
+
+terraform {
+  required_version = ">= 1.8"
+  required_providers {
+    panos = {
+      source = "PaloAltoNetworks/panos"
+      version = "~>1.11"
+    }
+    aws = {
+      version = "~>5.88"
+    }
+    google = {
+      version = "~>6.8"
+    }
+  }
+}
