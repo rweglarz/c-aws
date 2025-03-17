466c5a1e1438bc4b8649c7a623f33fb5de2a0a6f
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2025-03-17 20:53:10 +0100

    first function split and consume interfaces from passed config

--- modules/gwlb_asg_fw/lambda/asg_lambda.py
+++ modules/gwlb_asg_fw/lambda/asg_lambda.py
@@ -5,75 +5,27 @@ import os
 from datetime import datetime
 
 ec2_client = boto3.client('ec2')
-asg_client = boto3.client('autoscaling')
 
 
-def lambda_handler(event, context):
-    log(event)
-    log(context)
-    log(os.environ)
-    if os.environ['subnet_ids']:
-        subnet_ids = os.environ['subnet_ids'].split(",")
-    else:
-        log("Empty Environment variable subnet_ids:".format(os.environ['subnet_ids']))
-        exit(1)
-    if os.environ['interfaces']:
-        interfaces = json.loads(os.environ['interfaces'])
-    else:
-        log("Empty Environment variable interfaces")
-        exit(1)
-    if os.environ['di1_sg_ids']:
-        di1_sg_ids = os.environ['di1_sg_ids'].split(",")
-        log("di1_sg_ids:" + os.environ['di1_sg_ids'])
-    else:
-        log("Empty Environment variable di1_sg_ids:".format(os.environ['di1_sg_ids']))
-        exit(1)
-    if os.environ['di2_sg_ids']:
-        di2_sg_ids = os.environ['di2_sg_ids'].split(",")
-        log("di2_sg_ids:" + os.environ['di2_sg_ids'])
-    else:
-        log("Empty Environment variable di2_sg_ids:".format(os.environ['di2_sg_ids']))
-    ipv6 = False
-    try:
-        if os.environ['ipv6']=="true":
-            ipv6 = True
-            log("IPv6 enabled")
-        else:
-            log("IPv6 disabled")
-    except:
-        log("IPv6 not passed, not enabled")
-
-    if event["detail-type"] != "EC2 Instance-launch Lifecycle Action":
-        return
-
-    instance_id = event['detail']['EC2InstanceId']
-    instance_description = ec2_client.describe_instances(
-        InstanceIds=[instance_id])
+def handle_launch(instance_id, interfaces, config):
+    instance_description = ec2_client.describe_instances(InstanceIds=[instance_id])
     instance = instance_description['Reservations'][0]['Instances'][0]
     instance_zone = instance['Placement']['AvailabilityZone']
     log("Handling Launch for {} in {}".format(instance_id, instance_zone))
     # log("Disabling source/destination check")
     # ec2_client.modify_instance_attribute(SourceDestCheck={'Value': False}, InstanceId=instance_id)
 
-    device_index = 1
-    for subnet in subnet_ids:
-        zones = ec2_client.describe_subnets(SubnetIds=[subnet])
-        availability_zone = zones["Subnets"][0]["AvailabilityZone"]
-        if availability_zone != instance_zone:
-            continue
+    for device_index in range(1, len(interfaces)):
+        i_cfg = interfaces[str(device_index)]
+        subnet = i_cfg['subnet_id'][instance_zone]
         log("Adding interface index:{} in subnet: {}".format(device_index, subnet))
         
-        sg_ids = []
-        if device_index==1:
-            sg_ids = di1_sg_ids
-        if device_index==2:
-            sg_ids = di2_sg_ids
         ipv6_count = 0
-        if ipv6 and device_index==2:
+        if config['ipv6'] and device_index==2:
             ipv6_count = 1
         network_interface = ec2_client.create_network_interface(
                 SubnetId = subnet,
-                Groups = sg_ids,
+                Groups = i_cfg['security_group_ids'],
                 Description = subnet,
                 Ipv6PrefixCount = ipv6_count,
             )
@@ -91,7 +43,7 @@ def lambda_handler(event, context):
 
         associate_public_ip = False
         try:
-            associate_public_ip = interfaces[str(device_index)]['associate_public_ip']
+            associate_public_ip = i_cfg['associate_public_ip']
         except:
             pass
         if associate_public_ip:
@@ -107,7 +59,7 @@ def lambda_handler(event, context):
             log("Not associating public ip")
 
 
-        if ipv6:
+        if config['ipv6']:
             aipv6a = ec2_client.assign_ipv6_addresses(
                     NetworkInterfaceId=network_interface_id,
                     Ipv6AddressCount=1
@@ -122,8 +74,40 @@ def lambda_handler(event, context):
                 NetworkInterfaceId=network_interface_id,
             )
         log("debug: delete on termination: {}".format(mnia))
-        device_index += 1
 
+
+
+def lambda_handler(event, context):
+    log(event)
+    log(os.environ)
+    if os.environ['interfaces']:
+        interfaces = json.loads(os.environ['interfaces'])
+    else:
+        log("Empty Environment variable interfaces")
+        exit(1)
+    ipv6 = False
+    try:
+        if os.environ['ipv6']=="true":
+            ipv6 = True
+            log("IPv6 enabled")
+        else:
+            log("IPv6 disabled")
+    except:
+        log("IPv6 not passed, not enabled")
+    config = {}
+    config['ipv6'] = ipv6
+
+    instance_id = event['detail']['EC2InstanceId']
+    event_detail = event["detail-type"] 
+    if event_detail != "EC2 Instance-launch Lifecycle Action":
+        log(f"Instance: {instance_id} Event:{event_detail} - ignoring")
+        return
+
+    log(f"Instance: {instance_id} Event:{event_detail} - handling")
+
+    handle_launch(instance_id, interfaces, config)
+
+    asg_client = boto3.client('autoscaling')
     try:
         asg_client.complete_lifecycle_action(
                 LifecycleHookName=event['detail']['LifecycleHookName'],
@@ -131,7 +115,6 @@ def lambda_handler(event, context):
                 LifecycleActionToken=event['detail']['LifecycleActionToken'],
                 LifecycleActionResult='CONTINUE',
             )
-
     except botocore.exceptions.ClientError as e:
         log("Error completing life cycle hook for instance {}: {}".format(
             instance_id, e.response['Error']['Code']))
