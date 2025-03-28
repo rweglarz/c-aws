188cf6aa60cfef18fc64a6198af55d18e9c9ff61
Rafal Weglarz <rweglarz@paloaltonetworks.com>
2025-03-28 14:20:15 +0100

    chore: cloud-ngfw: update dns entries

--- cloud-ngfw/tgw-ngfw/dns.tf
+++ cloud-ngfw/tgw-ngfw/dns.tf
@@ -4,12 +4,12 @@ data "aws_route53_zone" "w-aws" {
 
 resource "aws_route53_record" "this" {
   for_each = {
-    cngfw-client1  = module.client1.public_ip
-    cngfw-attacker = aws_eip.attacker.public_ip
-    cngfw-victim   = aws_eip.victim.public_ip
+    client1  = module.client1.public_ip
+    attacker = aws_eip.attacker.public_ip
+    victim   = aws_eip.victim.public_ip
   }
   zone_id = data.aws_route53_zone.w-aws.zone_id
-  name    = each.key
+  name    = "${var.name}-${each.key}"
   type    = "A"
   ttl     = 300
   records = [
